07f4a42a6ed75e632f267caf033c6a797b759779
IDGB <49776938+ivergil@users.noreply.github.com>
2019-08-18 17:17:09 -0600

    Creating the router and export it

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,2 +1,37 @@
 var express = require("express");
 var burgerORM = require("../models/burger.js");
+
+var router = express.Router();
+//create routers and export them
+
+//select all burgers from the db and render them
+router.get("/", function(res) {
+    burgerORM.selectAll(function(data) {
+        res.render("index", {burger:data});
+ });
+});
+
+//post a new burger to the db
+router.post("/", function(req, res) {
+    burgerORM.insertOne(req.body.burger, function(result) {
+        console.log(result);
+        res.redirect("/");
+  });
+});
+
+//update a burger's devoured status
+router.put("/:id", function(req,res) {
+    burgerORM.updateOne(req.params.id, function(result) {
+        console.log(result);
+        res.redirect("/");
+  });
+});
+
+//delete a burger from the database
+router.delete("/:id", function(req,res) {
+    burgerORM.deleteOne(req.params.id, function(result) {
+      res.redirect("/");
+    });
+});
+
+module.exports = router;
