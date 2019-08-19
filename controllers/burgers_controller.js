f373edb0f84e78e003add915cb338f3e015ea216
IDGB <49776938+ivergil@users.noreply.github.com>
2019-08-18 18:40:33 -0600

    setting server.js

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -5,7 +5,7 @@ var router = express.Router();
 //create routers and export them
 
 //select all burgers from the db and render them
-router.get("/", function(res) {
+router.get("/", function(req, res) {
     burgerORM.selectAll(function(data) {
         res.render("index", {burger:data});
  });
@@ -15,6 +15,7 @@ router.get("/", function(res) {
 router.post("/", function(req, res) {
     burgerORM.insertOne(req.body.burger, function(result) {
         console.log(result);
+        console.log("entro por insertOne");
         res.redirect("/");
   });
 });
@@ -23,6 +24,7 @@ router.post("/", function(req, res) {
 router.put("/:id", function(req,res) {
     burgerORM.updateOne(req.params.id, function(result) {
         console.log(result);
+        console.log("entro por updateOne");
         res.redirect("/");
   });
 });
@@ -30,6 +32,7 @@ router.put("/:id", function(req,res) {
 //delete a burger from the database
 router.delete("/:id", function(req,res) {
     burgerORM.deleteOne(req.params.id, function(result) {
+        console.log("entro por deleteOne");
       res.redirect("/");
     });
 });
--- server.js
+++ server.js
@@ -1,28 +1,68 @@
-var express = require("express");
-var PORT = process.env.PORT || 8080;
+// var express = require("express");
+// var PORT = process.env.PORT || 8080;
 
-var app = express();
+// var app = express();
 
-// Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static("public"));
+// // Serve static content for the app from the "public" directory in the application directory.
+// app.use(express.static("public"));
 
-// Parse application body as JSON
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+// // Parse application body as JSON
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
 
-// Set Handlebars.
-var exphbs = require("express-handlebars");
+// // Set Handlebars.
+// var exphbs = require("express-handlebars");
 
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+// app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+// app.set("view engine", "handlebars");
+
+// // Import routes and give the server access to them.
+// var routes = require("./controllers/burgers_controller");
+
+// app.use(routes);
+
+// // Start our server so that it can begin listening to client requests.
+// app.listen(PORT, function() {
+//   // Log (server-side) when our server has started
+//   console.log("Server listening on: http://localhost:" + PORT);
+// });
+
+
+//set dependencies
+const express = require("express");
+const exphbs = require("express-handlebars");
+const bodyParser = require("body-parser");
+const methodOverride = require("method-override");
+
+//use app for an express call
+const app = express();
+
+//Set port, process.env.PORT for heroku, 3000 for local machine
+const PORT = process.env.PORT || 8080;
+
+//uese method override for forum PUT and DELETE queries
+app.use(methodOverride("_method"));
+
+//set engine and default for handlebars
+app.engine("handlebars", exphbs({ defaultLayout: "main"}) );
 app.set("view engine", "handlebars");
 
-// Import routes and give the server access to them.
-var routes = require("./controllers/catsController.js");
+//set body-parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
-app.use(routes);
+//serve static content from the public directory
+app.use(express.static(__dirname + "/public"));
 
-// Start our server so that it can begin listening to client requests.
-app.listen(PORT, function() {
-  // Log (server-side) when our server has started
-  console.log("Server listening on: http://localhost:" + PORT);
+//require burgers-controller,js for the routes
+const routes = require("./controllers/burgers_controller");
+
+app.use("/", routes);
+app.use("/:id", routes);
+
+//Initiate the listener
+app.listen(PORT, function(){
+  console.log("http://localhost:" + PORT);
 });
